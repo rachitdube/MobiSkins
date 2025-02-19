2e2c1a244e099c8fb231ad128bc4c165460ffc9b
Rachit <rachitdubey37@gmail.com>
2025-02-19 09:28:21 +0530

    env changes

--- MobiSkins/frontend/src/constants.js
+++ MobiSkins/frontend/src/constants.js
@@ -1,5 +1,7 @@
-// export const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:5000" : ""
-export const BASE_URL = "https://mobiskins-backend.onrender.com";
+export const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "https://mobiskins-backend.onrender.com"
+    : "";
 export const PRODUCTS_URL = "/api/products";
 export const USERS_URL = "/api/users";
 export const ORDERS_URL = "/api/orders";
